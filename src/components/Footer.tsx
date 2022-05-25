0741eb1f1d5ca86ae54fc0b9b66034f786234c51
Onur Kayhan <onur.kayhan@chasacademy.se>
2022-05-25 14:00:09 +0200

    footer to app

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-interface Footer {
-  text: string;
-}
+// interface Footer {
+//   text: string;
+// }
 
-export const Footer: React.FC<Footer> = () => {
+export const Footer: React.FC = () => {
   return (
     <footer className="bg-dark text-white pt-5 pb-4 fixed-bottom">
       <div className="container text-center text-md-left">
