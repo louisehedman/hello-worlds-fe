ebb2637b1f6758f9d288134138550135e3827ead
baldazay <alisher.sv88@gmail.com>
2022-05-27 20:55:17 +0200

    add header nav

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+
 root.render(
   <BrowserRouter>
     <Routes>
