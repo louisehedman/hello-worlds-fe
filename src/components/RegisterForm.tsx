c98e48c53c10531c927ba3b9c5f2452519a137bd
Nova Boman <nova.boman@chasacademy.se>
2022-05-27 15:29:11 +0200

    Create UserInfoInterface

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -1,13 +1,24 @@
 import axios from "axios";
 import React, { useState } from "react";
 
+interface UserInfoInterface {
+  firstName?: string;
+  username?: string;
+  email: string;
+  password: string;
+}
 interface PropsInterface {
   handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  state: UserInfoInterface;
+  setState: React.Dispatch<React.SetStateAction<UserInfoInterface>>;
 }
-const RegisterForm: React.FC<PropsInterface> = ({ handleChange }) => {
+
+const RegisterForm: React.FC<PropsInterface> = ({
+  handleChange,
+  state,
+  setState,
+}) => {
   const [value, setValue] = useState("");
-  // State
-  // Value
 
   //Parent state
   //
