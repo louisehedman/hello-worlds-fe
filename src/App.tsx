029688565465e41f4e4628e6a7bda60a2b47bd72
Nova Boman <nova.boman@chasacademy.se>
2022-05-27 22:36:14 +0200

    Refactor components

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,13 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React from "react";
 import "./App.css";
 import { Footer } from "./components/Footer";
-import Login from "./components/login/Login";
-import TripBlog from "./components/blog/tripblog";
-import PlanetSlider from "./components/slider";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  const [planets, setPlanets] = useState(null);
-
-  useEffect(() => {
-    if (!planets) {
-      try {
-        axios.get(`http://localhost:4000/planets`).then((response: any) => {
-          console.log(response);
-          setPlanets(response.data.planets);
-        });
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  });
-
   return (
     <div className="App">
-      {planets && <PlanetSlider planets={planets} />}
-      <TripBlog />
+      {/* NAVBAR GOES HERE */}
+      <Outlet />
       <Footer />
     </div>
   );
