67d2a1101cb0f4354eb264dd27be1b0f0642b701
Nova Boman <nova.boman@chasacademy.se>
2022-05-27 18:06:14 +0200

    Create temporary logout button and function

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,22 @@ import React from "react";
 import { Footer } from "./components/Footer";
 import "./App.css";
 import Login from "./components/Login";
+import axios from "axios";
 
 function App() {
+  const logout = async () => {
+    await fetch("http://localhost:4000/logout", {
+      method: "POST",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+
   return (
     <div className="App">
+      <button onClick={logout}>Logout</button>
       <Login />
       <Footer />
     </div>
