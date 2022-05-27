736c3b9b7c36f2c54e2c229347a35b56e8b4a27c
Nova Boman <nova.boman@chasacademy.se>
2022-05-27 22:29:10 +0200

    Refactor components

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import PlanetSlider from "../src/components/home/Slider";
+
+import TripBlog from "./tripblog";
 
 const Home: React.FC = () => {
-  return <div>Home</div>;
+  const [planets, setPlanets] = useState(null);
+
+  useEffect(() => {
+    if (!planets) {
+      try {
+        axios.get(`http://localhost:4000/planets`).then((response: any) => {
+          console.log(response);
+          setPlanets(response.data.planets);
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  });
+
+  return (
+    <>
+      {planets && <PlanetSlider planets={planets} />}
+      <TripBlog />
+    </>
+  );
 };
 
 export default Home;
