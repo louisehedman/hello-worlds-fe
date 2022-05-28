b35e2f4cd25ee2e98f319a57116527a7450c80a9
Nova Boman <nova.boman@chasacademy.se>
2022-05-28 13:39:33 +0200

    Move Header component to folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { Header } from "./components/Header";
+import { Header } from "./components/header/Header";
 import { Footer } from "./components/Footer";
 import { Outlet } from "react-router-dom";
 
