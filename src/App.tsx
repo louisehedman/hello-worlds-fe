a53426f75e6f180553af505de0b1d1b2d26beeae
Sandra Wallén <75799823+sandra-wallen@users.noreply.github.com>
2022-05-30 16:05:30 +0200

    Remove mounting of user in App (should be mounted in router)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-import User from "./pages/User";
 import "./App.css";
 import { Header } from "./components/header/Header";
 import { Footer } from "./components/Footer";
@@ -9,7 +8,6 @@ import { Outlet } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <User />
       <Header />
       <Outlet />
       <Footer />
