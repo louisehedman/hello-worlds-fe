4f1da92b0d7e97e673d776a80ebfa21e7c61f758
Sandra Wallén <75799823+sandra-wallen@users.noreply.github.com>
2022-05-30 16:09:18 +0200

    Add get trip url endpoint

--- src/reusable/urls.ts
+++ src/reusable/urls.ts
@@ -4,3 +4,5 @@ export const API_URL = (slug: string) => `${BASE_URL}/${slug}`
 export const GET_USER = (id: string) => `user/${id}`;
 
 export const GET_PLANET = (id: string | undefined) => `planets/${id}`;
+
+export const GET_TRIP = (userId: string | null, tripId: string | undefined) => `get-trip/${userId}/${tripId}`;
