086742c327dc367073beae0f4e8970c264d905be
louisehedman <louise.hedman@chasacademy.se>
2022-06-01 13:52:12 +0200

    remove console logs

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -14,7 +14,6 @@ const Login: React.FC = () => {
     let res = await axios.post(API_URL("logout"), {"body": "empty"}, {
       method: "POST", headers: {'Content-Type': 'application/json'}, withCredentials: true 
     });
-    console.log("res: ", res);
     if (res.status === 200) {
       console.log(res);
     } else {
@@ -37,7 +36,6 @@ const Login: React.FC = () => {
     e.preventDefault();
     try {
       let userInfo = state;
-      console.log("userinfo: ", userInfo);
       let res = await axios.post(url, userInfo, {method: 'POST', headers: {'Content-Type': 'application/json'}, withCredentials: true});
       console.log("res: ", res);
       if (res.status === 200) {
