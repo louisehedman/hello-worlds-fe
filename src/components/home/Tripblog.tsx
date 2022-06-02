0042c7fb7773d37fcd3ba7c124dd02cbc1d63779
Simon Miller <mail@simonmiller.se>
2022-06-02 20:35:33 +0200

    fix typo

--- src/components/home/Tripblog.tsx
+++ src/components/home/Tripblog.tsx
@@ -6,7 +6,7 @@ import { API_URL } from "../../reusable/urls";
 
 function TripBlog() {
   const [blogs, setBlogs] = useState([]);
-  const [currentPage, setCurentPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
   const postsPerPage = 3;
 
   useEffect(() => {
@@ -41,7 +41,7 @@ function TripBlog() {
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
   const currentPosts = blogs.slice(indexOfFirstPost, indexOfLastPost);
 
-  const paginate = (pageNumber: React.SetStateAction<number>) => setCurentPage(pageNumber)
+  const paginate = (pageNumber: React.SetStateAction<number>) => setCurrentPage(pageNumber)
 
   return (
     <div className="container px-4 pb-4">
