c688975f34b69282339a2662cccb7c8706f1f2bb
Sandra Wallén <75799823+sandra-wallen@users.noreply.github.com>
2022-06-02 23:12:45 +0200

    Update urls to take string or undefined instead of string or null

--- src/reusable/urls.ts
+++ src/reusable/urls.ts
@@ -1,11 +1,11 @@
 const BASE_URL = process.env.REACT_APP_API_URL;
 export const API_URL = (slug: string) => `${BASE_URL}/${slug}`;
 
-export const GET_USER = (id: string) => `user/${id}`;
+export const GET_USER = (id: string | undefined) => `user/${id}`;
 
 export const GET_PLANET = (id: string | undefined) => `planets/${id}`;
 
-export const GET_TRIP = (userId: string | null, tripId: string | undefined) =>
+export const GET_TRIP = (userId: string | undefined, tripId: string | undefined) =>
   `get-trip/${userId}/${tripId}`;
 
-export const CREATE_TRIP = (userId: string | null) => `create-trip/${userId}`;
+export const CREATE_TRIP = (userId: string | undefined) => `create-trip/${userId}`;
