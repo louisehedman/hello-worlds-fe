05ea37126f12c698a8059e51b297462f866fc5d1
louisehedman <louise.hedman@chasacademy.se>
2022-06-04 11:33:00 +0200

    try to solve conflict with react-range dependency

--- src/components/Destinations.tsx
+++ src/components/Destinations.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import axios from "axios";
 import { Range, getTrackBackground } from "react-range";
 import { Link } from "react-router-dom";
-import axios from "axios";
 import { API_URL } from "../reusable/urls";
 
 const Destinations: React.FC = () => {
