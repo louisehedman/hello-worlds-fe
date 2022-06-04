eb6a21a0be08a33d40bc8ddf2bcad2d96d4513a3
Nova Boman <nova.boman@chasacademy.se>
2022-06-05 01:27:10 +0200

    Update form for better UX, include state and handleSubmit in each form

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -1,35 +1,125 @@
-import React from "react";
-import { Props } from "./RegisterForm";
+import axios from "axios";
+import React, { useEffect, useRef, useState } from "react";
+import { UserDetailsInterface } from "../../interfaces/interfaces";
 import { API_URL } from "../../reusable/urls";
 
-const LoginForm: React.FC<Props> = ({ handleSubmit, handleChange, state }) => {
+const LoginForm: React.FC = () => {
+  // References to input elements
+  const emailRef = useRef<HTMLInputElement | null>(null);
+  const pwdRef = useRef<HTMLInputElement | null>(null);
+  // Reference to p displaying validation messages
+  const messageRef = useRef<HTMLParagraphElement | null>(null);
+  // Message variable
+  const [message, setMessage] = useState("");
+  // User input variable
+  const [state, setState] = useState<UserDetailsInterface>({
+    firstName: undefined,
+    username: undefined,
+    email: undefined,
+    password: undefined,
+  });
+
+  // Sets focus on the email field when component mounts
+  useEffect(() => {
+    if (emailRef.current) {
+      emailRef.current.focus();
+    }
+  }, []);
+
+  // Clears the error message when correcting email
+  useEffect(() => {
+    setMessage("");
+  }, [state.email]);
+
+  // Handles changes in inputs and updates state on each keypress
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setState({
+      ...state,
+      /* Event targets use the same name as state object properties.
+       * Sets the value of the current input to responding state property
+       */
+      [e.target.name]: value,
+    });
+  };
+
+  const handleSubmit = async (e: React.SyntheticEvent, url: string) => {
+    // Prevents reloading of page on submit
+    e.preventDefault();
+
+    try {
+      const res = await axios.post(
+        url,
+        { ...state },
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
+        }
+      );
+      // Clears the password field on submit
+      setState({ ...state, password: "" });
+      setMessage(`Welcome ${res.data.user}`);
+    } catch (err: any) {
+      // If password is invalid
+      if (err.response.status === 401) {
+        if (pwdRef.current) {
+          // Clear the password field and refocus password input
+          setState({ ...state, password: "" });
+          pwdRef.current.focus();
+        }
+      }
+      // If email is invalid
+      else if (emailRef.current) {
+        // Clear the password field and focus email input
+        setState({ ...state, password: "" });
+        emailRef.current.focus();
+      }
+      // Set message to error response message
+      setMessage(err.response.data);
+    }
+  };
+
   return (
-    <>
+    <div className="my-5">
+      <h2 className="text-white text-center">Sign in</h2>
+      <p
+        ref={messageRef}
+        className="text-white text-center"
+        style={!message ? { visibility: "hidden" } : { visibility: "visible" }}
+      >
+        {message}
+      </p>
+
       <form
-        className="w-50 m-auto"
+        className="w-100 m-auto"
         onSubmit={(e) => handleSubmit(e, API_URL("login"))}
       >
         <div className="form-group">
-          <label>Email:</label>
+          <label className="text-white">Email:</label>
           <input
-            className="form-control"
+            ref={emailRef}
+            className="form-control my-3"
             type="email"
             name="email"
             value={state.email}
             onChange={handleChange}
+            required
           />
-          <label>Password:</label>
+          <label className="text-white">Password:</label>
           <input
-            className="form-control"
+            ref={pwdRef}
+            className="form-control my-3"
             type="password"
             name="password"
             value={state.password}
             onChange={handleChange}
+            required
           />
           <input className="btn btn-light" type="submit" value="Login" />
         </div>
       </form>
-    </>
+    </div>
   );
 };
 export default LoginForm;
--- src/components/login/RegisterForm.tsx
+++ src/components/login/RegisterForm.tsx
@@ -1,61 +1,139 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useRef, useState } from "react";
 import { UserDetailsInterface } from "../../interfaces/interfaces";
 import { API_URL } from "../../reusable/urls";
-export interface Props {
-  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  handleSubmit: (e: React.SyntheticEvent, url: string) => void;
-  state: UserDetailsInterface;
-}
 
-const RegisterForm: React.FC<Props> = ({
-  handleChange,
-  handleSubmit,
-  state,
-}) => {
+const RegisterForm: React.FC = () => {
+  // References to input elements
+  const firstNameRef = useRef<HTMLInputElement | null>(null);
+  const emailRef = useRef<HTMLInputElement | null>(null);
+  const pwdRef = useRef<HTMLInputElement | null>(null);
+  // Reference to p displaying validation messages
+  const messageRef = useRef<HTMLParagraphElement | null>(null);
+  // Message variable
+  const [message, setMessage] = useState("");
+  // User input variable
+  const [state, setState] = useState<UserDetailsInterface>({
+    firstName: undefined,
+    username: undefined,
+    email: undefined,
+    password: undefined,
+  });
+
+  // Sets focus on the firstName field when component mounts
+  useEffect(() => {
+    if (firstNameRef.current) {
+      firstNameRef.current.focus();
+    }
+  }, []);
+
+  // Handles changes in inputs and updates state on each keypress
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setState({
+      ...state,
+      /* Event targets use the same name as state object properties.
+       * Sets the value of the current input to responding state property
+       */
+      [e.target.name]: value,
+    });
+  };
+
+  const handleSubmit = async (e: React.SyntheticEvent, url: string) => {
+    // Prevents reloading of page on submit
+    e.preventDefault();
+    // Set message instead of default validation error from server
+    if (state.password?.length) {
+      if (state.password.length < 8) {
+        // Refocus the password field
+        if (pwdRef.current) {
+          pwdRef.current.focus();
+        }
+        setState({ ...state, password: "" });
+        return setMessage("Password must be at least 8 characters");
+      }
+    }
+    try {
+      const res = await axios.post(
+        url,
+        { ...state },
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
+        }
+      );
+      console.log(res);
+      setMessage(`Registration successful`);
+    } catch (err: any) {
+      // For special type of database duplicate key error for unique fields
+      if (err.response.data.includes("E11000")) {
+        setMessage("Account already registered with this email");
+      } else {
+        setMessage(err.response.data);
+      }
+    }
+  };
+
   return (
-    <>
+    <div className="my-5">
+      <h2 className="text-white text-center">Register</h2>
+      <p
+        ref={messageRef}
+        className="text-white text-center"
+        style={!message ? { visibility: "hidden" } : { visibility: "visible" }}
+      >
+        {message}
+      </p>
+
       <form
-        className="w-50 m-auto"
+        className="w-100 m-auto"
         onSubmit={(e) => handleSubmit(e, API_URL("register"))}
       >
         <div className="form-group">
-          <label htmlFor="firstName">First Name:</label>
+          <label className="text-white">First name:</label>
           <input
-            className="form-control"
+            ref={firstNameRef}
+            className="form-control my-3"
             type="text"
-            id="firstName"
             name="firstName"
             value={state.firstName}
             onChange={handleChange}
+            required
           />
-          <label>Username:</label>
+          <label className="text-white">Username:</label>
           <input
-            className="form-control"
+            className="form-control my-3"
             type="text"
             name="username"
             value={state.username}
             onChange={handleChange}
+            required
           />
-          <label>Email:</label>
+          <label className="text-white">Email:</label>
           <input
-            className="form-control"
+            ref={emailRef}
+            className="form-control my-3"
             type="email"
             name="email"
             value={state.email}
             onChange={handleChange}
+            required
           />
-          <label>Password:</label>
+          <label className="text-white">Password:</label>
           <input
-            className="form-control"
+            ref={pwdRef}
+            className="form-control my-3"
             type="password"
             name="password"
             value={state.password}
             onChange={handleChange}
+            required
           />
           <input className="btn btn-light" type="submit" value="Register" />
         </div>
       </form>
-    </>
+    </div>
   );
 };
 export default RegisterForm;
