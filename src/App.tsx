a3a58893fe9249d28fdbb0668c78e0b24ff60ba5
Nova Boman <nova.boman@chasacademy.se>
2022-06-05 13:33:12 +0200

    Wrap all components in AuthProvider

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,35 @@ import { Outlet, useOutletContext } from "react-router-dom";
 import "./App.css";
 import { Header } from "./components/header/Header";
 import { Footer } from "./components/Footer";
+import AuthProvider from "./auth/AuthProvider";
 
 type ContextType = {
   userId: string | undefined;
   setUserId: React.Dispatch<React.SetStateAction<string | undefined>>;
-}
+};
 
 function App() {
   const session = localStorage.getItem("userId");
 
-  const [userId, setUserId] = useState<string | undefined>(session === null ? undefined : session);
+  const [userId, setUserId] = useState<string | undefined>(
+    session === null ? undefined : session
+  );
 
   return (
-    <div className="App" style={{display: "flex", flexDirection: "column", justifyContent: "space-between", minHeight: "100vh"}}>
-      <Header userId={userId}  />
-      <Outlet context={{ userId, setUserId }} />
-      <Footer />
+    <div
+      className="App"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        minHeight: "100vh",
+      }}
+    >
+      <AuthProvider>
+        <Header userId={userId} />
+        <Outlet context={{ userId, setUserId }} />
+        <Footer />
+      </AuthProvider>
     </div>
   );
 }
